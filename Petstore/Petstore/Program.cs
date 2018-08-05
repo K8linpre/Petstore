380586216bc1638b14f927627ec089ef8ee187a4
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-06 11:23:51 +1200

    Pet Store

---
+++ Petstore/Petstore/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petstore
+{
+    class Pet
+    {
+        static void Main(string[] args)
+        {
+            Program.Method();
+        }
+
+            string atype;
+            string gender;
+            string microchipped;
+            string breed;
+            float age;
+            float price;
+
+        public string AType
+        {
+            get
+            {
+                return atype;
+            }
+            set
+            {
+                atype = value;
+            }
+        }
+        public float Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+        public string Microchipped
+        {
+            get
+            {
+                return microchipped;
+            }
+            set
+            {
+                microchipped = value;
+            }
+        }
+        public string Breed
+        {
+            get
+            {
+                return breed;
+            }
+            set
+            {
+                breed = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    
+        public Pet(string _atype, float _age, string _gender, string _microchipped, string _breed, float _price)
+        {
+            AType = _atype;
+            Age = _age;
+            Gender = _gender;
+            Microchipped = _microchipped;
+            Breed = _breed;
+            Price = _price;
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Method()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Please Enter the Animal type");
+            string atype = (Console.ReadLine());
+            Console.WriteLine("The animal type is {0}", atype);
+        }
+    }
+}
