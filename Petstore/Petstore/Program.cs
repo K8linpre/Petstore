411a6332682fcb4f9b01422d646929f32f021554
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-08 09:31:06 +1200

    Pet Store

--- Petstore/Petstore/Program.cs
+++ Petstore/Petstore/Program.cs
@@ -13,7 +13,7 @@ namespace Petstore
             Program.Method();
         }
 
-            string atype;
+            string atype = "";
             string gender;
             string microchipped;
             string breed;
@@ -104,10 +104,28 @@ namespace Petstore
         public static void Method()
         {
 
-            Console.Clear();
+            
+
             Console.WriteLine("Please Enter the Animal type");
             string atype = (Console.ReadLine());
-            Console.WriteLine("The animal type is {0}", atype);
+            Console.WriteLine("Please Enter the Animal's age");
+            float age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter the Animal's gender");
+            string gender = (Console.ReadLine());
+            Console.WriteLine("Is the animal Microschipped (Yes/No)");
+            string microchipped = (Console.ReadLine());
+            Console.WriteLine("Please Enter the Animal's breed");
+            string breed = (Console.ReadLine());            
+            Console.WriteLine("Please Enter the Animal's price");
+            float price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pet List");
+            Console.WriteLine("Type: {0}", atype);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Gender: {0}", gender);
+            Console.WriteLine("Microchipped: {0}", microchipped);
+            Console.WriteLine("Breed: {0}",breed);
+            Console.WriteLine("Price: ${0}", price);
         }
     }
 }
